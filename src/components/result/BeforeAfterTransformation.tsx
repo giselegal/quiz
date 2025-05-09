44ded5a0c6dc8a4a5f63a37d607b323c1862252a
vdp2025 <gracielegslouback@gmail.com>
2025-05-09 21:03:21 +0000

    deploy

---
+++ src/components/result/BeforeAfterTransformation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BeforeAfterTransformation: React.FC = () => (
+  <div className="before-after-placeholder" style={{textAlign: 'center', padding: '2rem', color: '#B89B7A'}}>
+    {/* TODO: Substituir por conteúdo real */}
+    <h3>Transformações Antes e Depois</h3>
+    <p>Em breve, exemplos de transformação de estilo aqui!</p>
+  </div>
+);
+
+export default BeforeAfterTransformation;
